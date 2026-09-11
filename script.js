f72e4759949c9e7a63097e3f2eeb1cb2acf0d952
Nikhil Singh Katiyar <nikhildivya0777@gmail.com>
2026-09-11 13:09:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ import { GoogleGenAI } from "https://cdn.jsdelivr.net/npm/@google/genai/+esm";
 
 
 const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY
+    apiKey: import.meta.env.VITE_GEMINI_API_KEY
 });
 
 
